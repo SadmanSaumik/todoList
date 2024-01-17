eb9d03787aaef323e25fe0fa92de7c3e9a1c3582
SadmanSaumik <hemonm@gmail.com>
2024-01-17 23:23:52 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from "./components/Header.vue";
+import List from "./components/List.vue";
+import Addwork from "./components/AddWork.vue";
+</script>
+
+<template>
+  <Header></Header>
+  <div class="container">
+    <List></List>
+    <Addwork></Addwork>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>To Do List</h2>
+</template>
+<style scoped>
+h2 {
+  text-align: center;
+}
+</style>
